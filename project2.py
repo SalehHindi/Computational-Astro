3f75c08475b2357bd15334cad5c4b0e1fe0b8cd8
SalehHindi <shindi@haverford.edu>
2015-11-09 16:08:37 -0500

    Added vaccination function and fixed screen resizing issue

--- project2.py
+++ project2.py
@@ -16,18 +16,19 @@ roomSize=50
 numberOfPeople=50
 peopleLocation=0
 zombieLocation=0
+vaccLocation=[[25,25],[25,26],[26,25],[26,26]]
 
 fig=plott.figure()
 ax1=fig.add_subplot(1,1,1)
-axes = plott.gca()
-axes.set_xlim([0,roomSize])
-axes.set_ylim([0,roomSize])
 plott.ion()
-#plott.show()
-#change 50 to roomsize
+
+#the two variables below are so the pyplot screen doesn't constantly resize.
+#The white points indicate the room border 
+xborderpoint=[0,0,roomSize, roomSize]
+yborderpoint=[roomSize, 0, roomSize, 0]
 
 
-
+#Sets up a room of size roomSize and fills it with people.
 def createRoom():
 	randomLocation=[0,0]
 	people=[]
@@ -41,6 +42,8 @@ def createRoom():
 	#print people
 	return people
 
+#generateNewLocation() generates new location for every moving person. It checks whether they are in bounds on the room
+#and whether they are too close to a vaccinated person.
 def generateNewLocation(peopleLocation2):
 	newLocation=[0,1]
 	for personNumber in range(len(peopleLocation2)-1):
@@ -54,9 +57,11 @@ def generateNewLocation(peopleLocation2):
 					#print newLocation
 					break
 
-	print "New Location Set"
+#	print "New Location Set"
 	return peopleLocation2
 
+#checkInfected() checks whether a person within a certain pixel radius is infected and infects non infected people
+#within that radius based on some probability
 def checkInfected(people, zombie):
 	personNumber=0
 	while personNumber < len(peopleLocation)-1:
@@ -69,10 +74,11 @@ def checkInfected(people, zombie):
 						#print "personNumber:", personNumber, "\nlen(peopleLocation)", len(peopleLocation)
 						if peopleLocation[personNumber][0]+x==zombieCheck[0] and peopleLocation[personNumber][1]+y==zombieCheck[1]:
 							
+							#There is a 30% chance that someone will be infected if within range
 							if random.randint(1,10)>7: 
 								zombieLocation.append(people[personNumber])
 								peopleLocation.pop(personNumber)
-								print "Infected!"
+#								print "Infected!"
 							#print "XXXXXXX"
 					except:
 						#print "ERROR personNumber:", personNumber, "\nlen(peopleLocation)", len(peopleLocation)
@@ -80,26 +86,42 @@ def checkInfected(people, zombie):
 						personNumber+=-2
 						#raise SystemExit
 
+#plotting() is responsible for plotting the all the people in pyplot and animating the plot
+#Note: I should replace all the variables with one large list of length 3. That will make my code cleaner.
 def plotting(i):
 	xcoordpeople=[]
 	ycoordpeople=[]
 	xcoordzombie=[]
 	ycoordzombie=[]
+	xcoordvacc=[]
+	ycoordvacc=[]
 
 	ax1.clear()
-	for y in peopleLocation:
-		xcoordpeople.append(y[0])
-		ycoordpeople.append(y[1])
+	for x in peopleLocation:
+		xcoordpeople.append(x[0])
+		ycoordpeople.append(x[1])
 	#print peopleLocation
-	for x in zombieLocation:
-		xcoordzombie.append(x[0])
-		ycoordzombie.append(x[1])
-	
+	for y in zombieLocation:
+		xcoordzombie.append(y[0])
+		ycoordzombie.append(y[1])
+	for z in vaccLocation:
+		xcoordvacc.append(z[0])
+		ycoordvacc.append(z[1])
 
-	ax1.scatter(xcoordpeople, ycoordpeople)
+	ax1.scatter(xborderpoint,yborderpoint,c="white")
+	ax1.scatter(xcoordpeople, ycoordpeople,c="black")
 	ax1.scatter(xcoordzombie, ycoordzombie,c="red")
+	ax1.scatter(xcoordvacc, ycoordvacc,c="blue")
+	
 	plott.draw()	
 
+
+#vaccinate() will create a list of vaccinated people and specify their properties
+def vaccinate():
+	randomperson=random.randint(0,len(peopleLocation)-1)
+	vaccLocation.append(peopleLocation[randomperson])
+	peopleLocation.pop(randomperson)
+	
 
 #the locations of all noninfected people
 peopleLocation=createRoom()
@@ -116,6 +138,9 @@ for timeStep in range(7000):
 
 	#Check if anyone is infected
 	checkInfected(peopleLocation, zombieLocation)
+
+	#Vaccinate a random person every 3rd timeStep
+	if timeStep%3 == 0: vaccinate()
 
 	#Stop the movement of time when there are no non infected people
 	if len(peopleLocation) == 1:
